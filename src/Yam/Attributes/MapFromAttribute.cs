daaa7a5c1f7f50e53f76ad97070e3fc31ec31b6b
Raphael Dalmon <raphael.dalmon@epitech.eu>
2022-01-22 16:54:33 +0100

    Added MapFrom and MapTo attributes.

---
+++ src/Yam/Attributes/MapFromAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Yam.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class MapFromAttribute : Attribute
+{
+    public MapFromAttribute(params Type[] types)
+    {
+        Types = types;
+    }
+
+    public readonly Type[] Types;
+}
---
+++ src/Yam/Attributes/MapToAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Yam.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class MapToAttribute : Attribute
+{
+    public MapToAttribute(params Type[] types)
+    {
+        Types = types;
+    }
+
+    public readonly Type[] Types;
+}
